1727436c822715b9686ee5cf2b42b4532d3f78ab
joolof <joh.olofsson@gmail.com>
2019-10-03 11:34:44 -0300

    Polishing things

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-icon="$HOME/Pictures/Lock-icon.png"
-tmpbg='/tmp/screen.png'
+#icon="$HOME/Pictures/Lock-icon.png"
+#tmpbg='/tmp/screen.png'
 
-(( $# )) && { icon=$1; }
+#(( $# )) && { icon=$1; }
 
-scrot "$tmpbg"
-convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -u -i "$tmpbg" -n
-
+#scrot "$tmpbg"
+#convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+#convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+#i3lock -u -i "$tmpbg" -n
+i3lock -i /home/joo/.scripts/screen/lock.png -n -u -t
